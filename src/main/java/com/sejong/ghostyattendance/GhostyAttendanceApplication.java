da4299cf935b918f7458ec45bc4e4fc1bfcb413b
Miseong Kim <miseongk@naver.com>
2022-10-26 04:58:09 +0900

    initial commit

---
+++ src/main/java/com/sejong/ghostyattendance/GhostyAttendanceApplication.java
@@ -0,0 +1,13 @@
+package com.sejong.ghostyattendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhostyAttendanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhostyAttendanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sejong/ghostyattendance/GhostyAttendanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sejong.ghostyattendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhostyAttendanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
