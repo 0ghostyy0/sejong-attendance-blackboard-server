117e156856ff8b5ddcef2af8e2d9faf08eadfb6c
Miseong Kim <miseongk@naver.com>
2022-11-01 03:08:41 +0900

    feat: add lecture dto

---
+++ src/main/java/com/sejong/ghostyattendance/lecture/dto/LecturesOfCoursesRes.java
@@ -0,0 +1,17 @@
+package com.sejong.ghostyattendance.lecture.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class LecturesOfCoursesRes {
+
+    private String course_name;
+    private String course_id;
+    private String class_id;
+    private unpassCountRes unpass_count;
+    private List<LectureRes> lectures;
+}
---
+++ src/main/java/com/sejong/ghostyattendance/lecture/dto/unpassCountRes.java
@@ -0,0 +1,12 @@
+package com.sejong.ghostyattendance.lecture.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class unpassCountRes {
+
+    private int this_week;
+    private int all;
+}
