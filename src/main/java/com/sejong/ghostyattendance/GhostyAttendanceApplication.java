92e78754f7eff8135328dce452ca656fe4fec764
Miseong Kim <miseongk@naver.com>
2022-11-01 01:01:12 +0900

    chore: remove database dependency temporarily

--- src/main/java/com/sejong/ghostyattendance/GhostyAttendanceApplication.java
+++ src/main/java/com/sejong/ghostyattendance/GhostyAttendanceApplication.java
@@ -2,8 +2,9 @@ package com.sejong.ghostyattendance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class GhostyAttendanceApplication {
 
 	public static void main(String[] args) {
