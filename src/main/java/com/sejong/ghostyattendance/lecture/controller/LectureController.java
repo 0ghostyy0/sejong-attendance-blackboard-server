9f43e5af2aa9e568b5d77656c38083faf7071c70
Miseong Kim <miseongk@naver.com>
2023-01-15 14:55:34 +0900

    feat: add request log for LectureController

--- src/main/java/com/sejong/ghostyattendance/lecture/controller/LectureController.java
+++ src/main/java/com/sejong/ghostyattendance/lecture/controller/LectureController.java
@@ -3,11 +3,13 @@ package com.sejong.ghostyattendance.lecture.controller;
 import com.sejong.ghostyattendance.lecture.dto.CoursesReq;
 import com.sejong.ghostyattendance.lecture.dto.LecturesOfCoursesRes;
 import com.sejong.ghostyattendance.lecture.service.LectureService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/lectures")
 public class LectureController {
@@ -21,6 +23,7 @@ public class LectureController {
     @PostMapping("")
     @ResponseBody
     public ResponseEntity<List<LecturesOfCoursesRes>> getLectures(@RequestBody CoursesReq courses) {
+        log.info(log.getName());
         List<LecturesOfCoursesRes> lecturesOfCourses = lectureService.getLectures(courses);
 
         return ResponseEntity.ok(lecturesOfCourses);
